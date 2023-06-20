965b70292c70fcef04d1d83e27a53dc0954123ff
elleagn <133775323+elleagn@users.noreply.github.com>
2023-06-20 16:51:41 +0200

    Link the numeric buttons to the display
    Write function to populate the display when the user clicks the numeric buttons. Make the clear button clear the display.
    
    Write function to store the display as a binary tree.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # calculator
+
+Calculator assignment from the Odin's project.  
+Design inspired by the beautiful Sweet theme from EliverLara.  
+Fonts :  
+Neon Pixel-7 by Style-7
+Neon Glow by weknow
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*Functions to perform arithmetic operations*/
+
 const add = function (integer1, integer2) {
   return integer1 + integer2;
 };
@@ -17,17 +19,77 @@ const divide = function (integer1, integer2) {
   return integer1 / integer2;
 };
 
-let operand1;
-let operand2;
-let operator;
-let currentDisplay: querySelector(".calcul");
-
 const operate = function (operand1, operand2, operator) {
   return operator(operand1, operand2);
 };
 
+/*Functions to deal with expression tree*/
+
+const newLeaf = function (value = 0) {
+  return {
+    value,
+  };
+};
+
+function makeExpression(
+  operator,
+  firstOperand = newLeaf(),
+  secondOperand = newLeaf()
+) {
+  return {
+    operator,
+    firstOperand,
+    secondOperand,
+    value: operate(firstOperand.value, secondOperand.value, operator),
+  };
+}
+
+const appendOperatorBottom = function (tree, operator) {
+  switch (tree.operator) {
+    case undefined:
+      tree = makeExpression(operator, tree);
+      break;
+    default:
+      appendOperatorBottom(tree.secondOperand, operator);
+      tree.value = operate(
+        tree.firstOperand.value,
+        tree.secondOperand.value,
+        tree.operator
+      );
+  }
+};
+
+const appendOperator = function (position, operator) {
+  switch (position) {
+    case "top":
+      displayValue = makeExpression(operator, displayValue);
+      break;
+    case "bottom":
+      appendOperatorBottom(displayValue, operator);
+  }
+};
+
+/*Document variables*/
+const display = document.querySelector(".calcul");
 const numkeys = document.querySelectorAll(".num");
 const operatorkeys = document.querySelectorAll(".operator");
+const keys = document.querySelectorAll(".key");
+const equal = document.querySelector(".equal");
+const clear = document.querySelector(".clear");
+const tmpRes = document.querySelector(".res");
+
+/*Functions to update the display*/
+
+const addToDisplay = function (key) {
+  display.textContent += key.firstChild.textContent;
+};
+
+const clearDisplay = function () {
+  display.textContent = "";
+};
+
+let displayValue = newLeaf();
+updateDisplay();
 
 numkeys.forEach(function (key) {
   key.addEventListener("click", function (ev) {
@@ -35,6 +97,12 @@ numkeys.forEach(function (key) {
   });
 });
 
-function addToDisplay(key) {
-  ;
-}
+clear.addEventListener("click", clearDisplay);
+
+equal.addEventListener("click", () => {
+  display.textContent = tmpRes.textContent;
+  tmpRes.textContent = "";
+});
+
+let displayValue = newLeaf();
+updateDisplay();
