77b28646553141442ba9b0dba91094015e588923
elleagn <133775323+elleagn@users.noreply.github.com>
2023-06-25 18:38:09 +0200

    Clean the code
    Create fonction for tree actions with better name and simpler usage.

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
+/*J'ai essayé de coder les parentheses il faut les fonctions pour les déclencher et après les utiliser pour mettre les puissances et les arrondis. Pour fermer la parenthese il faut juste mettre parenthese à false*/
+
 /*Functions to perform arithmetic operations*/
 
 const add = function (integer1, integer2) {
+  integer1 = integer1 === null ? 0 : integer1;
+  integer2 = integer2 === null ? 0 : integer2;
   return integer1 + integer2;
 };
 
 const subtract = function (integer1, integer2) {
+  integer1 = integer1 === null ? 0 : integer1;
+  integer2 = integer2 === null ? 0 : integer2;
   return integer1 - integer2;
 };
 
 const multiply = function (integer1, integer2) {
+  integer1 = integer1 === null ? 1 : integer1;
+  integer2 = integer2 === null ? 1 : integer2;
   return integer1 * integer2;
 };
 
 const divide = function (integer1, integer2) {
+  if (integer2 === null) {
+    integer2 = 1;
+  }
   if (integer2 === 0) {
     return "Division by zero, go directly to math jail, do not collect 200$.";
   }
@@ -20,77 +31,144 @@ const divide = function (integer1, integer2) {
 };
 
 const operate = function (operand1, operand2, operator) {
-  return operator(operand1, operand2);
+  return convertToFunction(operator)(operand1, operand2);
+};
+
+const power = function (base, exponent) {
+  if (exponent === null) {
+    exponent = 1;
+  }
+
+  switch (exponent) {
+    case 0:
+      return 1;
+    default:
+      return base * power(base, exponent - 1);
+  }
+};
+
+const round = function (number, pow) {
+  if (pow <= 13) {
+    return number;
+  }
+  return Math.round(number / power(10, pow - 2)) / 10;
+};
+
+const priority = function (operator) {
+  switch (operator) {
+    case "power":
+      return 4;
+
+    case "x":
+      return 3;
+
+    case "/":
+      return 2;
+
+    case "+":
+    case "-":
+      return 1;
+  }
 };
 
 /*Functions to deal with expression tree*/
 
-const isALeaf = function (expression) {
-  return expression.operator === undefined;
+const isANumber = function (expression) {
+  return !expression.hasOwnProperty("operator");
 };
 
-function makeExpression(operator, firstOperand = 0, secondOperand = 0) {
-  const expression = {
-    operator,
-    firstOperand: { value: firstOperand },
-    secondOperand: { value: secondOperand },
-    value: 0,
-  };
-  expression.lastOperatorNode = isALeaf(secondOperand)
-    ? expression
-    : secondOperand.lastOperatorNode;
-  updateValue(expression);
-
-  return expression;
+function createNumber(value = null) {
+  return { value: value };
 }
 
-const addNumber = function (number) {
-  if (isALeaf(displayValue)) {
-    displayValue.value = 10 * displayValue.value + number;
+const latestNumber = function (expression) {
+  if (isANumber(expression)) {
+    return expression;
+  }
+  return latestNumber(expression.secondOperand);
+};
+
+function joinExpressions(operator, firstOperand, secondOperand) {
+  if (isANumber(firstOperand)) {
+    firstOperand.operator = operator;
+    firstOperand.firstOperand = { value: firstOperand.value };
+    firstOperand.secondOperand = secondOperand;
   } else {
-    displayValue.lastOperatorNode.secondOperand.value =
-      10 * displayValue.lastOperatorNode.secondOperand.value + number;
+    let tmpExpression = {
+      operator: firstOperand.operator,
+      value: firstOperand.value,
+      firstOperand: firstOperand.firstOperand,
+      secondOperand: firstOperand.secondOperand,
+    };
+    firstOperand.operator = operator;
+    firstOperand.firstOperand = tmpExpression;
+    firstOperand.secondOperand = secondOperand;
+  }
+  firstOperand.value = operate(
+    firstOperand.firstOperand.value,
+    firstOperand.secondOperand.value,
+    firstOperand.operator
+  );
+}
+
+const addDigit = function (node, digit) {
+  if (!node.value) {
+    node.value = digit;
+  } else if (Number.isNaN(node.value)) {
+    alert("Node isn't a number");
+  } else {
+    node.value = 10 * node.value + digit;
   }
 };
 
-const updateValue = function (tree, force = false) {
-  if (!isALeaf(tree)) {
-    updateValue(tree.secondOperand);
-    if (tree.operator === "/") {
-      if (force && tree.secondOperand.value === 0) {
-        clearDisplay();
-        display.classList.add("small");
-        addToDisplay("FATAL ERROR:Division by zero. You go to math jail!");
-        return "error";
-      } else if (tree.secondOperand.value !== 0) {
-        tree.value = operate(
-          tree.firstOperand.value,
-          tree.secondOperand.value,
-          convertToFunction(tree.operator)
-        );
-      }
+const addOperator = function (operator, expression) {
+  if (
+    isANumber(expression) ||
+    priority(operator) <= priority(expression.operator)
+  ) {
+    joinExpressions(operator, expression, createNumber());
+  } else {
+    addOperator(operator, expression.secondOperand);
+    return expression;
+  }
+};
+
+const updateValues = function (expression) {
+  if (!isANumber(expression)) {
+    updateValues(expression.secondOperand);
+    if (expression.operator === "/" && expression.secondOperand.value === 0) {
+      expression.value = operate(
+        expression.firstOperand.value,
+        1,
+        expression.operator
+      );
     } else {
-      tree.value = operate(
-        tree.firstOperand.value,
-        tree.secondOperand.value,
-        convertToFunction(tree.operator)
+      expression.value = operate(
+        expression.firstOperand.value,
+        expression.secondOperand.value,
+        expression.operator
       );
     }
   }
 };
 
-const appendOperatorAux = function (expression, operator) {
-  switch (isALeaf(expression)) {
-    case true:
-      expression.firstOperand = { value: expression.value };
-      expression.secondOperand = { value: 0 };
-      expression.lastOperatorNode = expression;
-      expression.operator = operator;
-      expression.lastOperatorNode = expression;
-      break;
-    case false:
-      appendOperatorAux(expression.secondOperand, operator);
-      expression.lastOperatorNode = expression.secondOperand.lastOperatorNode;
+const calculateValue = function (expression) {
+  if (!isANumber(expression)) {
+    if (calculateValue(expression.secondOperand) === "error") {
+      return "error";
+    }
+    if (expression.operator === "/" && expression.secondOperand.value === 0) {
+      clearDisplay();
+      display.classList.add("small");
+      addToDisplay("FATAL ERROR:Division by zero. You go to math jail!");
+      return "error";
+    } else {
+      expression.value = operate(
+        expression.firstOperand.value,
+        expression.secondOperand.value,
+        expression.operator
+      );
+    }
   }
 };
 
@@ -104,58 +182,64 @@ const convertToFunction = function (operatorString) {
       return multiply;
     case "/":
       return divide;
-  }
-};
-
-const appendOperator = function (operator) {
-  switch (operator) {
-    case "-":
-    case "+":
-      displayValue = makeExpression(operator, displayValue.value);
-      break;
-    case "x":
-      appendOperatorAux(displayValue, operator);
-      break;
-    case "/":
-      if (
-        isALeaf(displayValue) ||
-        !(convertToFunction(displayValue.lastOperatorNode.operator)(2, 3) === 6)
-      ) {
-        appendOperatorAux(displayValue, operator);
-      } else {
-        displayValue.lastOperatorNode.firstOperand =
-          displayValue.lastOperatorNode;
-        displayValue.lastOperatorNode.secondOperand = { value: 0 };
-        displayValue.lastOperatorNode.operator = operator;
-      }
+    case "round":
+      return round;
   }
 };
 
 /*Document variables*/
-const display = document.querySelector(".calcul");
+const mainExpression = document.querySelector(".main-expression");
 const numkeys = document.querySelectorAll(".num");
 const operatorkeys = document.querySelectorAll(".operator");
 const keys = document.querySelectorAll(".key");
 const pad = document.querySelector(".pad");
 const equal = document.querySelector(".equal");
 const clear = document.querySelector(".clear");
-const tmpRes = document.querySelector(".res");
+const tmpResult = document.querySelector(".tmp-result");
 const minusKey = document.querySelector(".minus");
+let mainExpressionTree = createNumber();
+let parentheses = [mainExpressionTree];
+let mainNode = mainExpressionTree;
+
 /*Functions to update the display*/
 
+const updateTmp = function () {
+  value = mainExpressionTree.value;
+  length = value.toString().length;
+  tmpResult.textContent =
+    length > 10
+      ? (
+          Math.round(value / power(10, length - 1)) * power(10, length - 1)
+        ).toExponential()
+      : value;
+};
+
 const clearDisplay = function () {
-  display.classList.remove("small");
-  display.classList.add("big");
-  display.textContent = "";
-  tmpRes.textContent = "";
-  displayValue = { value: 0 };
+  mainExpression.classList.remove("small");
+  mainExpression.classList.add("big");
+  mainExpression.textContent = "";
+  tmpResult.textContent = "";
+  mainExpressionTree = { value: null };
+  parentheses = [mainExpressionTree];
+  mainNode = mainExpressionTree;
 };
 
 const confirmExpression = function () {
-  if (updateValue(displayValue, true) !== "error") {
-    display.textContent = displayValue.value;
-    displayValue = { value: displayValue.value };
-    tmpRes.textContent = "";
+  if (calculateValue(mainExpression) !== "error") {
+    const value = mainExpressionTree.value;
+    const length = value.toString().length;
+    mainExpressionTree = {
+      value:
+        length > 10
+          ? Math.round(value / power(10, length - 1)) * power(10, length - 1)
+          : value,
+    };
+    mainExpression.textContent =
+      length > 10 ? mainExpressionTree.value.toExponential() : value;
+
+    tmpResult.textContent = "";
+    parentheses = [mainExpressionTree];
+    mainNode = mainExpressionTree;
   }
 };
 
@@ -163,7 +247,7 @@ const addToDisplay = function (keyValue) {
   if (keyValue === "x") {
     keyValue = "*";
   }
-  display.textContent += keyValue;
+  mainExpression.textContent += keyValue;
 };
 
 const findSpan = function (key) {
@@ -174,17 +258,7 @@ const findSpan = function (key) {
   return keySpan;
 };
 
-const updateExpression = function (keyValue, type) {
-  switch (type) {
-    case "operator":
-      appendOperator(keyValue);
-      break;
-    case "number":
-      addNumber(Number(keyValue));
-  }
-};
-
-const reactToKeyPress = function (ev) {
+const reactToKeyClick = function (ev) {
   const key = ev.currentTarget;
   let keySpan = findSpan(key);
 
@@ -196,28 +270,32 @@ const reactToKeyPress = function (ev) {
     case "=":
       confirmExpression();
       enableOperators();
+      disableNumkeys();
+      operatorkeys.forEach((key) => {
+        key.removeEventListener("click", closeParenthesis, { once: true });
+      });
       break;
     case "+":
     case "x":
     case "/":
     case "-":
-      updateExpression(keySpan.textContent, "operator");
+      addOperator(keySpan.textContent, mainNode);
       disableOperators();
       addToDisplay(keySpan.textContent);
       break;
+    case "(":
+      openParentheses();
+      addToDisplay(keySpan.textContent);
+      disableOperators();
+    case ")":
+      closeParenthesis();
+      addToDisplay(keySpan.textContent);
+      enableOperators();
+
     default:
-      updateExpression(keySpan.textContent, "number");
-      updateValue(displayValue);
-      if (
-        !isALeaf(displayValue) &&
-        (displayValue.operator !== "/" ||
-          displayValue.secondOperand.value !== 0)
-      ) {
-        tmpRes.textContent = displayValue.value;
-      }
-      if (Number(display) === NaN) {
-        clearDisplay();
-      }
+      addDigit(latestNumber(mainNode), Number(keySpan.textContent));
+      updateValues(mainExpressionTree);
+      updateTmp();
       addToDisplay(keySpan.textContent);
       enableOperators();
   }
@@ -226,28 +304,41 @@ const reactToKeyPress = function (ev) {
 /*Functions to enable/disable the operators*/
 const activateNumkeys = function () {
   numkeys.forEach(function (key) {
-    key.addEventListener("click", reactToKeyPress);
+    key.addEventListener("click", reactToKeyClick);
     key.addEventListener("mouseover", animateKeys);
   });
 };
 
+const disableNumkeys = function () {
+  numkeys.forEach(function (key) {
+    key.removeEventListener("click", reactToKeyClick);
+    key.removeEventListener("mouseover", animateKeys);
+  });
+  operatorkeys.forEach(function (key) {
+    key.addEventListener("click", () => {
+      activateNumkeys();
+    });
+  });
+};
+
 const activateMisc = function () {
-  clear.addEventListener("click", clearDisplay);
-  equal.addEventListener("click", confirmExpression);
+  clear.addEventListener("click", reactToKeyClick);
+  equal.addEventListener("click", reactToKeyClick);
   equal.addEventListener("mouseover", animateKeys);
   clear.addEventListener("mouseover", animateKeys);
 };
 
 const enableOperators = function () {
   operatorkeys.forEach(function (key) {
-    key.addEventListener("click", reactToKeyPress, { once: true });
+    key.addEventListener("click", reactToKeyClick, { once: true });
+    minusKey.removeEventListener("click", handleNegative, { once: true });
     key.addEventListener("mouseover", animateKeys);
   });
 };
 
 const disableOperators = function () {
   operatorkeys.forEach(function (key) {
-    key.removeEventListener("click", reactToKeyPress, { once: true });
+    key.removeEventListener("click", reactToKeyClick, { once: true });
   });
   enableNegative();
 };
@@ -258,26 +349,42 @@ const enableNegative = function () {
 };
 
 const handleNegative = function (ev) {
-  if (!isALeaf(displayValue)) {
+  if (!isANumber(mainNode)) {
     addToDisplay("(-");
     rememberParenthesis();
   } else {
     addToDisplay("-");
   }
-  appendOperator("x");
-  displayValue.lastOperatorNode.firstOperand.value = -1;
+  addDigit(latestNumber(mainNode), -1);
+  addOperator("x");
+};
+
+const disableNegative = function () {
+  minusKey.removeEventListener("click", handleNegative, { once: true });
 };
 
 const rememberParenthesis = function () {
   operatorkeys.forEach(function (key) {
-    key.addEventListener("click", closeParenthesis);
+    key.addEventListener("click", closeParenthesis, { once: true });
   });
+};
+
+const openParentheses = function () {
+  parentheses += latestNumber(mainNode);
+  mainNode = parentheses[parentheses.length - 1];
 };
 
 const closeParenthesis = function () {
   addToDisplay(")");
+  calculateValue(mainNode);
+  if (parentheses.pop() === mainExpressionTree) {
+    joinExpressions(null, mainExpressionTree, createNumber());
+    parentheses = [mainExpressionTree];
+  } else {
+    mainNode = parentheses[parentheses.length - 1];
+  }
   operatorkeys.forEach(function (key) {
-    key.removeEventListener("click", closeParenthesis);
+    key.removeEventListener("click", closeParenthesis, { once: true });
   });
 };
 
@@ -325,7 +432,7 @@ const chooseColor = function (keyType) {
 const animateKeys = function (ev) {
   const key = ev.currentTarget;
   const color = chooseColor(findKeyType(key));
-  let angle = { value: 0 };
+  let angle = { value: null };
   intervalID = setInterval(turnAround, 2, key, angle, color);
   key.addEventListener(
     "mouseout",
@@ -337,9 +444,44 @@ const animateKeys = function (ev) {
   );
 };
 
-/*Initializing the calculator*/
+/*Dealing with overflow*/
 
+const popLatestNumber = function () {
+  const displayReversed = mainExpression.textContent
+    .split("")
+    .reverse()
+    .join("");
+  let number = "";
+  for (i of displayReversed) {
+    if (Number(i) === "NaN") {
+      return number;
+    } else {
+      number = mainExpression.textContent.pop() + number;
+    }
+  }
+  return number;
+};
+
+const doNotOverflow = function (ev) {
+  if (
+    mainExpression.offsetWidth >
+      mainExpression.parentElement.offsetWidth - 25 &&
+    mainNode === mainExpressionTree
+  ) {
+    disableNumkeys();
+    disableOperators();
+  }
+};
+
+numkeys.forEach(function (key) {
+  key.addEventListener("click", doNotOverflow);
+});
+
+/*Implementation of parentheses*/
+
+/*Initializing the calculator*/
 clearDisplay();
 activateNumkeys();
 activateMisc();
 enableNegative();
+pad.addEventListener("click", () => {});
